2209a4cc54bece622f96c9c2ee7475f4171a883c
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-15 16:28:41 +0300

    initial commit

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,12 +2,29 @@ import Modal from "./Modal";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resdata = await response.json();
+      setPosts(resdata.posts);
+    }
+    fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(postData),
+    });
+
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
   return (
@@ -17,11 +34,23 @@ function PostList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      <ul className={classes.posts}>
-        {posts.map((post) => (
-          <Post author={post.author} body={post.body} />
-        ))}
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post
+              key={(post = Math.random())}
+              author={post.author}
+              body={post.body}
+            />
+          ))}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white " }}>
+          <h2>No posts yet</h2>
+          <p>Start add some! click on new post Button</p>
+        </div>
+      )}
     </>
   );
 }
